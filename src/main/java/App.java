8bdb0cb03f3924f080aaeafc2ee1d984d3910fcb
Collin Person and Rich Garrick <rich@>
2017-08-08 14:03:32 -0700

    no errors

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,22 +19,36 @@ public class App {
             return new ModelAndView(model, "index.hbs");
         }, new HandlebarsTemplateEngine());
 
-        get("/form", (request, response) -> {
-            Map<String,Object> model = new HashMap<String, Object>();
-            return new ModelAndView(model, "form.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        post("/posts/add", (request, response) -> {
+        post("/success", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            String albumName = request.queryParams("albumName");
-            Tracker newTracker = new Tracker(albumName);
-            return new ModelAndView(model, "form.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/success", (request, response) -> {
-            Map<String,Object> model = new HashMap<String, Object>();
+            String formInput = request.queryParams("formInput");
+            Tracker newTracker = new Tracker(formInput);
             return new ModelAndView(model, "success.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/add", (request, response) -> {
+            Map<String,Object> model = new HashMap<String, Object>();
+            return new ModelAndView(model, "form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get ("/view", (request, response) -> {
+            Map<String,Object>model = new HashMap<>();
+            ArrayList<Tracker> trackers = Tracker.getAll();
+            model.put("instances",trackers);
+
+            return new ModelAndView(model, "view.hbs");
+        },   new HandlebarsTemplateEngine());
+//        post("/success", (request, response) -> {
+//            Map<String,Object> model = new HashMap<String, Object>();
+//            return new ModelAndView(model, "success.hbs");
+//        }, new HandlebarsTemplateEngine());
+//
+//        get("/delete", (request, response) -> {
+//            Map<String,Object> model = new HashMap<String, Object>();
+//            return new ModelAndView(model, "delete.hbs");
+//        }, new HandlebarsTemplateEngine());
+
+
 
 
     }
